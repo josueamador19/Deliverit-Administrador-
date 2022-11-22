af1537d85ec688278f5ffadb54c97833ffda6451
CoFlores-12 <obethflores2014@gmail.com>
2022-11-22 16:29:26 -0600

    Connected to the backend (stores module only)

--- assets/js/stores.js
+++ assets/js/stores.js
@@ -1,55 +1,65 @@
-var dataStore = [];
+var stores = [];
 var catSelect = "Show all";
-
-let categories = ['Restaurants', 'Supermarket', 'Drinks', 'Health', 'Tech'];
+let categories = [];
 
 (async ()=>{
-    categories.forEach((category, indexCat) => {
+    renderCategories();
+    renderStores();
+})();
+
+async function renderCategories() {
+    let settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "http://localhost:3000/admin/categories",
+        "method": "GET",
+        "headers": {}
+    };
+    
+    await $.ajax(settings).done(function (response) {
+        categories = response
+    });
+    
+    $('#categoriesContainer').html('<button class="btn activeCat SA" onclick="filterSelection(this)"> Show all</button>');
+
+    categories.forEach(category => {
         $('#categoriesContainer').append(
-            '<button class="btn" id="indexCat'+indexCat+'" onclick="filterSelection(this)">'+category+'</button>'
+            '<button class="btn" id="'+category._id+'" onclick="filterSelection(this)">'+category.name+'</button>'
         );
     });
+
     $('#categoriesContainer').append(
         '<button class="btn addCat" onclick="openCatModal()"><i class="fa-solid fa-plus"></i></button>'
     );
+}
 
-    for (let i = 0; i < 10; i++) {
-        const prts = []
-        let sumSales = 0;
+async function renderStores() {
+    settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "http://localhost:3000/admin/stores",
+        "method": "GET",
+        "headers": {}
+      };
+      
+    await $.ajax(settings).done(function (response) {
+        stores = response
+    });
 
-        for (let j = 0; j < Math.floor(Math.random() * 10)+1; j++) {
-            let sales = Math.floor(Math.random() * 1000);
-            prts.push({
-                name: 'Product '+ (j+1),
-                price: Number(Math.random() * 100).toFixed(2),
-                sales: sales
-            });
-            sumSales+=sales;
-        }
-        dataStore.push({
-            name:'Store '+i,
-            category:categories[Math.floor(Math.random() * 5)],
-            sales: sumSales,
-            products: prts
-        })
-    }
 
     $('#tableStoresBody').html('');
 
-    dataStore.forEach((store, ind) => {
+    stores.forEach((store, ind) => {
         $('#tableStoresBody').append(
             `<tr>
             <td>${store['name']}</td>
-            <td>${store['category']}</td>
-            <td>${store['sales']}</td>
-            <td>${store['products'].length}</td>
+            <td>${store['category'].name}</td>
+            <td>${store.products.length}</td>
             <td class="row center-xy"><button onclick="viewModalPro(this)" data-store="${ind}" class="btn editBtn"><i class="fa-solid fa-pen-to-square"></i></button></td>
         </tr>`
         )
     });
-
-
-})();
+}
 
 function searchStore() {
     const textInput = $('#searchStore').val();
@@ -57,14 +67,13 @@ function searchStore() {
         return
     }
     $('#tableStoresBody').html('');
-    dataStore.forEach((store, ind) => {
+    stores.forEach((store, ind) => {
         if ((store['name'].toLowerCase().includes(textInput.toLowerCase()))) {
             $('#tableStoresBody').append(
                 `<tr>
                 <td>${store['name']}</td>
-                <td>${store['category']}</td>
-                <td>${store['sales']}</td>
-                <td>${store['products'].length}</td>
+                <td>${store['category'].name}</td>
+                <td>${store.products.length}</td>
                 <td class="row center-xy"><button onclick="viewModalPro(this)" data-store="${ind}" class="btn editBtn"><i class="fa-solid fa-pen-to-square"></i></button></td>
             </tr>`
             )
@@ -74,13 +83,20 @@ function searchStore() {
 
 let indexStore=0;
 let indexProduct=0;
+let idStore = '';
 
 function viewModalPr(product) {
     $('#modalPr').css('display', 'flex');
-    const ind = $(product).attr('data-product');
-    indexProduct = ind
-    $('#namePrt').val(dataStore[indexStore]['products'][ind]['name']);
-    $('#pricePr').val(dataStore[indexStore]['products'][ind]['price']);
+    indexProduct = null;
+    if (product !== null) {
+        const ind = $(product).attr('data-product');
+        indexProduct = ind
+        $('#namePrt').val(stores[indexStore]['products'][ind]['name']);
+        $('#pricePr').val(stores[indexStore]['products'][ind]['price']);
+        $('#descriptionPr').val(stores[indexStore]['products'][ind]['description']);
+        $('#imgPr').val(stores[indexStore]['products'][ind]['img']);
+        $('#previewPr').attr("src",stores[indexStore]['products'][ind]['img']);
+    }
 }
 
 function viewModalPro(store) {
@@ -89,18 +105,23 @@ function viewModalPro(store) {
     $('#categoriesStore').html('')
     $('#products').html('')
     categories.forEach(cat =>{
-        $('#categoriesStore').append(`<option value="${cat}">${cat}</option>`)
+        $('#categoriesStore').append(`<option value="${cat._id}">${cat.name}</option>`)
     })
     indexStore=null
     if (store != null) {
         $('.AP').css('display', 'block');
         const ind = $(store).attr('data-store');
         indexStore = ind;
-        $('#namePr').val(dataStore[ind]['name']);
-        $("#categoriesStore").val(dataStore[ind]['category']);
+        idStore = stores[ind]._id;
+        $('#namePr').val(stores[ind]['name']);
+        $("#categoriesStore").val(stores[ind]['category'].id);
+        $('#logoPr').val(stores[ind]['logo'])
+        $('#previewLogo').attr("src",stores[ind]['logo'])
+        $('#bannerPr').val(stores[ind]['banner'])
+        $('#previewBanner').attr("src",stores[ind]['banner'])
         const productsData = []
-        dataStore[ind]['products'].forEach((products, index) => {
-            productsData.push([products.name, products.price, products.sales, index])
+        stores[ind]['products'].forEach((products, index) => {
+            productsData.push([products.name, products.price, index])
         })
         const grid = new gridjs.Grid({
             columns: [
@@ -118,7 +139,7 @@ function viewModalPro(store) {
                 },
                 {
                     name: 'Actions',
-                    formatter: (_, row) => gridjs.html(`<div class="btnAct"><button onclick="viewModalPr(this)" data-product="${row.cells[3].data}" class="btn editBtn"><i class="fa-solid fa-pen-to-square"></i></button></div>`)
+                    formatter: (_, row) => gridjs.html(`<div class="btnAct"><button onclick="viewModalPr(this)" data-product="${row.cells[2].data}" class="btn editBtn"><i class="fa-solid fa-pen-to-square"></i></button></div>`)
                 },
             ],
             search: true,
@@ -138,21 +159,19 @@ function viewModalPro(store) {
     
 }
 
-
 function filterSelection(filter) {
     $('.activeCat').removeClass('activeCat');
     $(filter).addClass('activeCat');
     catSelect = $(filter).html();
 
     $('#tableStoresBody').html('');
-    dataStore.forEach((store, ind) => {
-        if (store['category'] === $(filter).html() || $(filter).html().trim() === "Show all".trim()) {
+    stores.forEach((store, ind) => {
+        if (store['category'].name === $(filter).html() || $(filter).html().trim() === "Show all".trim()) {
             $('#tableStoresBody').append(
                 `<tr>
                 <td>${store['name']}</td>
-                <td>${store['category']}</td>
-                <td>${store['sales']}</td>
-                <td>${store['products'].length}</td>
+                <td>${store['category'].name}</td>
+                <td>${store.products.length}</td>
                 <td class="row center-xy"><button onclick="viewModalPro(this)" data-store="${ind}" class="btn editBtn"><i class="fa-solid fa-pen-to-square"></i></button></td>
             </tr>`
             )
@@ -169,18 +188,21 @@ function closeCatModal() {
 }
 
 function addCat() {
-    const nameCat = $('#nameCatAdd').val();
-    if (nameCat == '') {
+    const nameCat = $('#nameCatAddName').val();
+    const colorCat = $('#nameCatAddColor').val();
+    const iconCat = $('#nameCatAddIcon').val();
+    if (nameCat == '' || colorCat == '' || iconCat == '') {
         return
     }
-    categories.push(nameCat);
-    $('.addCat').remove();
-    $('#categoriesContainer').append(
-        '<button class="btn" onclick="filterSelection(this)">'+nameCat+'</button>'
-    );
-    $('#categoriesContainer').append(
-        '<button class="btn addCat" onclick="openCatModal()"><i class="fa-solid fa-plus"></i></button>'
-    );
+    $.post("http://localhost:3000/admin/newCategory", {
+        name: nameCat,
+        color: colorCat,
+        icon: iconCat, }).done(function (response) {
+            $('.addCat').remove();
+            renderCategories()
+        }).fail(function(xhr, status, res) {
+
+            })
     closeCatModal();
 }
 
@@ -193,41 +215,93 @@ function viewDeleteCatModal() {
 }
 
 function deleteCategory() {
-    dataStore.forEach((store, i) => {
-        if(store.category === $('.activeCat').html()){
-            dataStore.splice(i, 1);
-        }
-    })
-    const index = categories.indexOf($('.activeCat').html());
-    categories.splice(index, 1);
+    const category = categories.find(element => element.name === $('.activeCat').html());
+    const data = {
+        "idCategory": category._id }
+const settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "http://localhost:3000/admin/deleteCategory",
+        "method": "DELETE",
+        data: data}
+      
+$.ajax(settings).done(function (response) {
+    renderCategories();
     $('.activeCat').remove();
     $('.SA').addClass('activeCat');
     filterSelection($('.SA'))
     $('#modalDeleteCat').css('display', 'none');
+});
+   
 }
 
 function saveEditStore() {
     if (indexStore == null) {
-        dataStore.push({
-            name:$('#namePr').val(),
-            category:$("#categoriesStore").val(),
-            sales: 0,
-            products: []
-        })
+          $.post("http://localhost:3000/admin/newStore", {
+            name: $('#namePr').val(),
+            logo: $('#logoPr').val(),
+            banner: $('#bannerPr').val(),
+            category: $('#categoriesStore').val(), }).done(function (response) {
+                renderStores()
+            }).fail(function(xhr, status, res) {
+
+            })
     }else{
-        dataStore[indexStore]['name'] = $('#namePr').val();
-        dataStore[indexStore]['category'] = $('#categoriesStore').val();
+        const data = {
+                "idStore": idStore,
+                "name": $('#namePr').val(),
+                "logo": $('#logoPr').val(),
+                "banner": $('#bannerPr').val(),
+                "category": $('#categoriesStore').val(), }
+        const settings = {
+                "async": true,
+                "crossDomain": true,
+                "url": "http://localhost:3000/admin/updateStore",
+                "method": "PUT",
+                data: data}
+              
+        $.ajax(settings).done(function (response) {
+                renderStores()
+        });
     }
     $('#modalStore').css('display', 'none');
     filterSelection($('.activeCat'))
 }
 
-function saveProduct() {
-    dataStore[indexStore]['products'][indexProduct]['name'] =  $('#namePrt').val();
-    dataStore[indexStore]['products'][indexProduct]['price'] =  $('#pricePr').val();
+async function saveProduct() {
+    if (indexProduct == null) {
+        await $.post("http://localhost:3000/admin/newProduct", {
+            "img": $('#imgPr').val(),
+            "price": $('#pricePr').val(),
+            "description": $('#descriptionPr').val(), 
+            "idStore": idStore, 
+            "name": $('#namePr').val(),}).done(function (response) {
+              renderStores()
+          }).fail(function(xhr, status, res) {
+
+          })
+  }else{
+    const idProductCS = stores[indexStore]['products'][indexProduct];
+      const data = {
+          "img": $('#imgPr').val(),
+          "price": $('#pricePr').val(),
+          "description": $('#descriptionPr').val(), 
+          "idProduct": idProductCS._id, 
+          "name": $('#namePrt').val(),}
+      const settings = {
+              "async": true,
+              "crossDomain": true,
+              "url": "http://localhost:3000/admin/updateProduct",
+              "method": "PUT",
+              data: data}
+            
+      await $.ajax(settings).done(function (response) {
+            renderStores()
+      });
+  }
     const productsData = []
-        dataStore[indexStore]['products'].forEach((products, index) => {
-            productsData.push([products.name, products.price, products.sales, index])
+        stores[indexStore]['products'].forEach((products, index) => {
+            productsData.push([products.name, products.price, index])
         })
         const grid = new gridjs.Grid({
             columns: [
@@ -263,4 +337,39 @@ function saveProduct() {
         grid.render(document.getElementById("products"));
         grid.forceRender();
         $('#modalPr').css('display', 'none');
+}
+
+function deleteStore() {
+    const store = stores[indexStore];
+    const data = {
+        "idStore": store._id }
+const settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "http://localhost:3000/admin/deleteStore",
+        "method": "DELETE",
+        data: data}
+      
+$.ajax(settings).done(function (response) {
+    renderCategories();
+    $('#modalStore').css('display', 'none');
+});
+}
+
+function deleteProduct() {
+    const product = stores[indexStore]['products'][indexProduct];
+    const data = {
+        "idProduct": product._id }
+const settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "http://localhost:3000/admin/deleteProduct",
+        "method": "DELETE",
+        data: data}
+      
+$.ajax(settings).done(function (response) {
+    renderStores();
+    $('#modalPr').css('display', 'none');
+    $('#modalStore').css('display', 'none');
+});
 }
